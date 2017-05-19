a522f278a0cc79f7be49264df3872ebb49681f21
Evgeny Yulyugin <yulyugin@gmail.com>
2017-05-19 14:12:40 +0200

    Introduce personal dictionary for russian.

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 
 script_path=`pwd`
 
-rm ~/.emacs
-rm -r ~/.emacs.d/lisp
+rm_f="rm -rf"
+
+$rm_f ~/.emacs
+$rm_f ~/.emacs.d/lisp
+$rm_f ~/.emacs.d/ispell
 
 ln -s $script_path/.emacs ~/
 ln -s $script_path/.emacs.d/lisp ~/.emacs.d/
+ln -s $script_path/ispell ~/.emacs.d/
