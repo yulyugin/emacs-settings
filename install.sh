4950d1e306a144e620d5f9178a21be25e88ff399
Evgeny Yulyugin <yulyugin@gmail.com>
2017-05-20 17:48:52 +0200

    Fix installation script.

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ $rm_f ~/.emacs
 $rm_f ~/.emacs.d/lisp
 $rm_f ~/.emacs.d/ispell
 
+mkdir -p ~/.emacs.d
+
 ln -s $script_path/.emacs ~/
 ln -s $script_path/.emacs.d/lisp ~/.emacs.d/
 ln -s $script_path/.emacs.d/ispell ~/.emacs.d/
